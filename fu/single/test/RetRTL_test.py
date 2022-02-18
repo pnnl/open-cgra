5fa72d6ed2f1764b499ba1efc9707a9e635b62ae
tancheng <tancheng1990@gmail.com>
2022-02-17 22:20:08 -0800

    [update] control flow

--- fu/single/test/RetRTL_test.py
+++ fu/single/test/RetRTL_test.py
@@ -1,24 +1,21 @@
 """
 ==========================================================================
-CompRTL_test.py
+RetRTL_test.py
 ==========================================================================
-Test cases for functional unit Phi.
+Test cases for functional unit Ret.
 
 Author : Cheng Tan
-  Date : November 27, 2019
+  Date : September 21, 2021
 
 """
 
-from pymtl3                         import *
-from pymtl3.stdlib.test             import TestSinkCL
-from pymtl3.stdlib.test.test_srcs   import TestSrcRTL
+from pymtl3                       import *
+from pymtl3.stdlib.test           import TestSinkCL
+from pymtl3.stdlib.test.test_srcs import TestSrcRTL
 
-from ..CompRTL                      import CompRTL
-from ....lib.opt_type               import *
-from ....lib.messages               import *
-
-from pymtl3.passes.backends.verilog import TranslationPass, VerilatorImportPass, TranslationImportPass
-from pymtl3.passes.PassGroups       import *
+from ..RetRTL                     import RetRTL
+from ....lib.opt_type             import *
+from ....lib.messages             import *
 
 #-------------------------------------------------------------------------
 # Test harness
@@ -28,13 +25,12 @@ class TestHarness( Component ):
 
   def construct( s, FunctionUnit, DataType, PredicateType, CtrlType,
                  num_inports, num_outports, data_mem_size,
-                 src_data, src_ref, src_predicate, src_opt, sink_msgs ):
+                 src_in, src_predicate, src_opt, sink ):
 
-    s.src_data      = TestSrcRTL( DataType,      src_data      )
-    s.src_ref       = TestSrcRTL( DataType,      src_ref       )
+    s.src_in        = TestSrcRTL( DataType,      src_in        )
     s.src_predicate = TestSrcRTL( PredicateType, src_predicate )
     s.src_opt       = TestSrcRTL( CtrlType,      src_opt       )
-    s.sink_out      = TestSinkCL( DataType,      sink_msgs     )
+    s.sink          = TestSinkCL( DataType,      sink          )
 
     s.dut = FunctionUnit( DataType, PredicateType, CtrlType,
                           num_inports, num_outports, data_mem_size )
@@ -42,14 +38,13 @@ class TestHarness( Component ):
     for i in range( num_inports ):
       s.dut.recv_in_count[i] //= 1
 
-    connect( s.src_data.send,      s.dut.recv_in[0]     )
-    connect( s.src_ref.send,       s.dut.recv_in[1]     )
+    connect( s.src_in.send,        s.dut.recv_in[0]     )
     connect( s.src_predicate.send, s.dut.recv_predicate )
     connect( s.src_opt.send,       s.dut.recv_opt       )
-    connect( s.dut.send_out[0],    s.sink_out.recv      )
+    connect( s.dut.send_out[0],    s.sink.recv          )
 
   def done( s ):
-    return s.src_data.done() and s.sink_out.done()
+    return s.src_opt.done() and s.sink.done()
 
   def line_trace( s ):
     return s.dut.line_trace()
@@ -60,6 +55,7 @@ def run_sim( test_harness, max_cycles=100 ):
   test_harness.sim_reset()
 
   # Run simulation
+
   ncycles = 0
   print()
   print( "{}:{}".format( ncycles, test_harness.line_trace() ))
@@ -69,31 +65,30 @@ def run_sim( test_harness, max_cycles=100 ):
     print( "{}:{}".format( ncycles, test_harness.line_trace() ))
 
   # Check timeout
+
   assert ncycles < max_cycles
 
   test_harness.tick()
   test_harness.tick()
   test_harness.tick()
 
-def test_Comp():
-  FU = CompRTL
-  DataType      = mk_data( 32, 1 )
+def test_Ret():
+  FU            = RetRTL
+  DataType      = mk_data( 16, 1 )
   PredicateType = mk_predicate( 1, 1 )
   CtrlType      = mk_ctrl()
   num_inports   = 2
-  num_outports  = 1
+  num_outports  = 2
   data_mem_size = 8
   FuInType      = mk_bits( clog2( num_inports + 1 ) )
-  pickRegister  = [ FuInType( x+1 ) for x in range( num_inports ) ]
-  src_data      = [ DataType(9, 1), DataType(3, 1), DataType(3, 1) ]
-  src_ref       = [ DataType(9, 1), DataType(5, 1), DataType(2, 1) ]
-  src_predicate = [ PredicateType(1,0), PredicateType(1,0), PredicateType(1,1) ]
-  src_opt       = [ CtrlType( OPT_EQ, b1( 0 ), pickRegister ),
-                    CtrlType( OPT_LE, b1( 0 ), pickRegister ),
-                    CtrlType( OPT_EQ, b1( 0 ), pickRegister ) ]
-  sink_out      = [ DataType(1, 1), DataType(1, 1), DataType(0, 1) ]
+  src_in        = [ DataType(1, 1), DataType(2, 1), DataType(3, 1) ]
+  src_predicate = [ PredicateType(1, 0), PredicateType(1,0), PredicateType(1,1) ]
+  src_opt       = [ CtrlType( OPT_RET, b1( 1 ), [FuInType(1), FuInType(0)] ),
+                    CtrlType( OPT_RET, b1( 0 ), [FuInType(1), FuInType(0)] ),
+                    CtrlType( OPT_RET, b1( 1 ), [FuInType(1), FuInType(0)] ) ]
+  sink          = [ DataType(1, 0), DataType(2, 1), DataType(3, 1) ]
   th = TestHarness( FU, DataType, PredicateType, CtrlType,
                     num_inports, num_outports, data_mem_size,
-                    src_data, src_ref, src_predicate, src_opt, sink_out )
+                    src_in, src_predicate, src_opt, sink )
   run_sim( th )
 
